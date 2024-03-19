4f0a95cd60625e816e2472322ff58dcc98f00e81
Jordan Hall <Jordan@libertyware.co.uk>
2024-03-19 09:53:50 +0000

    merge history

--- angular-pkgs/ng2-search-filter/src/lib/import/src/ng2-filter.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Ng2SearchPipe } from './ng2-filter.pipe';
-
-@NgModule({
-  declarations: [Ng2SearchPipe],
-  exports: [Ng2SearchPipe]
-})
-
-export class Ng2SearchPipeModule { }
