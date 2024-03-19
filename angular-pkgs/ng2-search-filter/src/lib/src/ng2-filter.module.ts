d31664b075850c65b31eebd473a77c7258b2c7ef
Jordan Hall <Jordan@libertyware.co.uk>
2024-03-19 09:52:41 +0000

    Import standalone-repo into ng2-filter

---
+++ angular-pkgs/ng2-search-filter/src/lib/src/ng2-filter.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { Ng2SearchPipe } from './ng2-filter.pipe';
+
+@NgModule({
+  declarations: [Ng2SearchPipe],
+  exports: [Ng2SearchPipe]
+})
+
+export class Ng2SearchPipeModule { }
